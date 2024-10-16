878b9e5fb9f233545a5b093154bfaffb20682ced
Te Manawa o Cambridge <admin@cambridgeyouthcentre.org.nz>
2024-10-16 14:14:38 +1300

    configure the graphql playground in production

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,53 +1,58 @@
-const express = require('express')
+const express = require('express');
 const { ApolloServer } = require('apollo-server-express')
-const cors = require('cors') // Enable CORS
-// const helmet = require('helmet') // Security middleware
-// const bodyParser = require('body-parser')
+const cors = require('cors'); // Enable CORS
+const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core')
 
 // GraphQL type definitions and resolvers
-const typeDefs = require('./graphql/typeDefs')
-const resolvers = require('./graphql/resolvers')
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
 
 // Create an instance of ApolloServer
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  persistedQueries: false
-})
+  persistedQueries: false,
+  introspection: true, // Enable introspection for Apollo Sandbox in production
+  plugins: [
+    ApolloServerPluginLandingPageGraphQLPlayground(), // Enable GraphQL Playground
+  ],
+});
 
-const app = express()
+const app = express();
 
 // Set up CORS to allow requests from both your production website and localhost (for development)
-const allowedOrigins = ['https://temanawaocambridge.org.nz', `http://localhost:${process.env.PORT}`]
+const allowedOrigins = [
+  process.env.PRODUCTION_WEBSITE_URL,
+  process.env.PRODUCTION_GRAPHQL_URL,
+  `http://localhost:${process.env.PORT || 4000}`,
+  'http://localhost:3001' // dev website url
+].filter(Boolean)
 
 const corsOptions = {
   origin: function (origin, cb) {
     // Allow requests with no origin (like mobile apps, curl, Postman, etc.) or from allowed domains
     if (!origin || allowedOrigins.includes(origin)) {
-      cb(null, true)
+      cb(null, true);
     } else {
-      cb(new Error('Not allowed by CORS'))
+      console.error('Not allowed by CORS', origin)
+      cb(new Error('Not allowed by CORS',))
     }
   },
-  credentials: true // This allows cookies and other credentials to be sent from the client
-}
+  credentials: true, // This allows cookies and other credentials to be sent from the client
+};
 
-app.use(cors(corsOptions)) // Apply CORS middleware
+app.use(cors(corsOptions)); // Apply CORS middleware
 
-// Add additional middleware (security, body parsing)
-// app.use(helmet())
-// app.use(bodyParser.json())
+async function startApolloServer() {
+  await server.start(); // Start Apollo Server
+  server.applyMiddleware({ app }); // Apply Apollo middleware to the Express app
 
-async function startApolloServer () {
-  await server.start() // Start Apollo Server
-  server.applyMiddleware({ app }) // Apply Apollo middleware to the Express app
-
-  console.log('Apollo Server is set up and middleware applied')
+  console.log('Apollo Server is set up and middleware applied');
 }
 
 // Call the function to start Apollo Server
-startApolloServer().catch(err => {
-  console.error('Error starting Apollo Server: ', err)
-})
+startApolloServer().catch((err) => {
+  console.error('Error starting Apollo Server: ', err);
+});
 
-module.exports = app // Export the app so server.js can use it
+module.exports = app; // Export the app so server.js can use it
